30ddb4c62d921d487c6d568bcea028af09e20f7f
devwiandj@gmail.com <devWianDJ@gmail.com>
2023-02-26 19:54:43 +0200

    added uitls folder with files and function for image display

--- src/utils/productUtils.js
+++ src/utils/productUtils.js
@@ -1,24 +1,21 @@
-import tent from '../resources/images/products/tent.jpg'
-import pack from '../resources/images/products/pack.jpg'
-import hikingboots from '../resources/images/products/hikingboots.jpg'
-import fishingrod from '../resources/images/products/fishingrod.jpg'
-import fishingreel from '../resources/images/products/fishingreel.jpg'
-import campingchair from '../resources/images/products/campingchair.jpg'
+import tent from "../resources/images/products/tent.jpg";
+import pack from "../resources/images/products/pack.jpg";
+import hikingboots from "../resources/images/products/hikingboots.jpg";
+import fishingrod from "../resources/images/products/fishingrod.jpg";
+import fishingreel from "../resources/images/products/fishingreel.jpg";
+import campingchair from "../resources/images/products/campingchair.jpg";
 
-
-
-
-const images = {
-  tent,
-  pack,
-  hikingboots,
-  fishingrod,
-  fishingreel,
-  campingchair,
-};
 
 function getProductImageByKey(key) {
-  return images[key]
+  const images = {
+    tent,
+    pack,
+    hikingboots,
+    fishingrod,
+    fishingreel,
+    campingchair,
+  };
+  return images.key;
 }
 
-export default getProductImageByKey
+export default getProductImageByKey;
